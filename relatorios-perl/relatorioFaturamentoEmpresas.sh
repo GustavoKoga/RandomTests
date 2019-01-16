cfc49a563657fba8aeb2d844b86a7f0509f62f56
GustavoKoga <g_k_n@live.com>
2019-01-16 15:02:41 -0200

    Commit inicial de códigos aleatorios

--- relatorios-perl/relatorioFaturamentoEmpresas.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-perl relatorioFaturamentoEmpresas.pl 1 '2018-11-01' '2018-11-30' > relatorioEmpresasM01.csv
-perl relatorioFaturamentoEmpresas.pl 2 '2018-11-01' '2018-11-30' > relatorioEmpresasM02.csv
